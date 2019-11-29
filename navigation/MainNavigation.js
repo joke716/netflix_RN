5f1890aef20cbb51748312be9b8388e067216f6d
Teddy Kwak <joke716@gmail.com>
2019-11-29 17:22:30 +0900

    Detail Screen #2

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -2,7 +2,7 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import TabNavigation from "./TabNavigation";
-import DetailScreen from "../screens/Detail";
+import DetailScreen from "./../screens/Detail";
 import {headerStyles} from "./config";
 
 const MainNavigation = createStackNavigator(
